a2603809c30204487b09605946cc677ea9c8d7c8
Nicholas Kauldhar <1559905@NOAM.TCS.com>
2018-08-20 14:16:24 -0400

    Fixed certificate generation

--- src/main/java/com/policy/dao/PolicyDao.java
+++ src/main/java/com/policy/dao/PolicyDao.java
@@ -5,11 +5,13 @@
 package com.policy.dao;
 
 import java.sql.*;
+import java.time.LocalDate;
 import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
 import java.util.ArrayList;
 
+import com.policy.data.Nominee;
 import com.policy.data.Policy;
 
 /*
@@ -289,6 +291,7 @@ public class PolicyDao {
 		
 	}
 
+
 	/**
 	 * Method used by Admin to generate certificates. It uses customer and 
 	 * policy ID to find a PolicyMap. It then stores the ID of that policy map
@@ -323,7 +326,22 @@ public class PolicyDao {
 			ResultSet rs = st.executeQuery("Select * from PolicyMap where customer_id = " 
 			+ c + " and policy_id = " + d);
 			if(rs.next()) {
-				request.getSession().setAttribute("certificateMapID", rs.getInt(1));
+				PolicyDao dao = new PolicyDao();
+				Date date = rs.getDate(5);
+				Policy pol = dao.selectAllPolicyByID(d);
+				
+				LocalDate ld = LocalDate.parse(date.toString());
+				System.out.println(ld.toString());
+				ld = ld.plusYears((long)pol.getTenure());
+				
+				date = Date.valueOf(ld);
+				List<Nominee> noms = NomineeDao.getNomineesFromMapID(rs.getInt(1));
+				System.out.println("size: " + noms.size());
+				request.getSession().setAttribute("CertPremium", rs.getDouble(7));
+				request.getSession().setAttribute("CertNominees", noms);				
+				request.getSession().setAttribute("CertEndDate", date);
+				request.getSession().setAttribute("CertPolicy", pol);
+				
 				return true;
 			}
 			else {
@@ -333,6 +351,7 @@ public class PolicyDao {
 		
 		}
 		catch (Exception e) {
+			e.printStackTrace();
 			return false;
 		}
 		finally {
